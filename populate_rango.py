b5f9a8ee47bf7c6179c8e8dbbc9430978ed10091
Eleonora Della <2244079D@student.gla.ac.uk>
2018-01-22 16:59:22 +0000

    End of Chapter 6

--- populate_rango.py
+++ populate_rango.py
@@ -9,41 +9,31 @@ from rango.models import Category, Page
 def populate():
     
     python_pages = [
-		{"title": "Official Python Tutorial", "url":"http://docs.python.org/2/tutorial/"},
-		{"title":"How to Think like a Computer Scientist", "url":"http://www.greenteapress.com/thinkpython/"},
-	    {"title":"Learn Python in 10 Minutes", "url":"http://www.korokithakis.net/tutorials/python/"}  ]
+		{"title": "Official Python Tutorial", "url":"http://docs.python.org/2/tutorial/", "views": 23},
+		{"title":"How to Think like a Computer Scientist", "url":"http://www.greenteapress.com/thinkpython/", "views": 32},
+	    {"title":"Learn Python in 10 Minutes", "url":"http://www.korokithakis.net/tutorials/python/","views": 6 }  ]
     
     django_pages = [
 		{"title":"Official Django Tutorial",
-	        "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
+	        "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/", "views": 25},
 		{"title":"Django Rocks",
-	        "url":"http://www.djangorocks.com/"},
+	        "url":"http://www.djangorocks.com/" ,"views": 30},
 		{ "title":"How to Tango with Django",
-	        "url":"http://www.tangowithdjango.com/"}]
+	        "url":"http://www.tangowithdjango.com/", "views": 22}]
     
     other_pages = [
-		{ "title":"Bottle", "url":"http://bottlepy.org/docs/dev/"},
-		{ "title":"Flask", "url":"http://flask.pocoo.org"} ]
+		{ "title":"Bottle", "url":"http://bottlepy.org/docs/dev/", "views": 32},
+		{ "title":"Flask", "url":"http://flask.pocoo.org", "views": 16} ]
     
     cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
 	    "Django": {"pages": django_pages, "views": 64, "likes": 32},
             "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16}
             }
     
-        # if you want to add more catergories or pages, add them to the dictionaries above
-	# The code below goes through the cats dictionary, then adds each category,
-	# and then adds all the associated pages for that category
-	# if you are using Python 2.x then use cats.iteritems() see
-	# http://docs.quantifiedcode.com/python-anti-patterns/readability/not_using_items_to_iterate_over_a_dictionary.html
-	# for more information about using items() and how to iterate over a dictionary properly
-    
-    # Using the .items returns the key and the value. In this case the key is "Python", "Django" or "Other Frameworks" and the value (cat_data) is the corresponding dictionary in cats.
     for cat, cat_data in cats.items():
-        # c = add_cat(cat)
-        # Updated the population script to pass through the specific values for views and likes
         c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"])
+            add_page(c, p["title"], p["url"], p[views])
     
     # Print out what we have added to the user.
     for c in Category.objects.all():
